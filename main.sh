d45954fd3d0faae30937397e303dedaa15399677
faraz-epigentek <fahmed02@icloud.com>
2020-03-19 00:06:04 -0400

    notes

--- main.sh
+++ main.sh
@@ -60,6 +60,7 @@ sort(){
 }
 
 
+
 markDups(){
 		
 
@@ -69,7 +70,7 @@ markDups(){
         do
             iSUB=`echo $i | cut -d "." -f1`
             
-            java -jar /programs/bin/picard-tools/picard.jar \
+            picard \
             MarkDuplicates \
             INPUT=$i \
             OUTPUT=${iSUB}.dupMarked.sorted.bam \
@@ -80,40 +81,79 @@ markDups(){
             TMP_DIR=tmp
 
         done
-		
+        mkdir dupMarked-BAMS
+        mv *.dupMarked.sorted.bam dupMarked-BAMS
+
 		cd ..
 
-
 }
 
 
 
 
-alignBowtie2(){
+# alignBowtie2(){
 
-	cd trimmed_fastqs
+# 	cd trimmed_fastqs
 
-	for i in *.gz 
-	do
-		iSUB=`echo $i | cut -d "_" -f1`
+# 	for i in *.gz 
+# 	do
+# 		iSUB=`echo $i | cut -d "_" -f1`
 		
-		(bowtie2 \
-		--local \
-        -x /Volumes/TOSHIBA/SQ190110D–R6_Colombia/Analysis/Homo_sapiens/UCSC/hg38/Sequence/Bowtie2Index/genome \
-        -U $i \
-        -S ${iSUB}.sam ) 2>${i}.log
+# 		(bowtie2 \
+# 		--local \
+#         -x /Volumes/TOSHIBA/SQ190110D–R6_Colombia/Analysis/Homo_sapiens/UCSC/hg38/Sequence/Bowtie2Index/genome \
+#         -U $i \
+#         -S ${iSUB}.sam ) 2>${i}.log
     
-    done
+#     done
 
-    source activate multiqc 
-    multiqc -n bowtie2 .
+#     source activate multiqc 
+#     multiqc -n bowtie2 .
 
-	cd ..
+# 	cd ..
 
+
+# }
+
+# alignBowtie2
+
+
+
+
+dedupBAM(){
+
+	cd primary-BAMS/dupMarked-BAMS
+	# alignment stats etc. on dupMarked no MT bams
+	
+	for i in *.dupMarked.sorted.bam
+	
+	do
+	
+	iSUB=`echo $i | cut -d "." -f1`
+	samtools view -b -h -F 0X400 $i > ${iSUB}.DEDUP.bam
+	
+	done
+
+	for i in *.DEDUP.bam; do samtools index $i ; samtools idxstats $i > `echo $i | cut -d "." -f1`.DEDUP.idxstats; done
+	for i in *.DEDUP.bam; do samtools flagstat $i > `echo $i | cut -d "." -f1`.DEDUP.flagstat; done
+
+	
+	mkdir dedup-BAMS
+	mv *.DEDUP* dedup-BAMS/
+	mv dedup-BAMS ../../
+	
+	cd ../..
 
 }
 
-alignBowtie2
+
+# macs2 callpeak -t 2815-C.DEDUP.bam \
+# 	-c 2815-I.DEDUP.bam \
+#  	-f BAM -g 2.7e9 \
+# 	-n 2815-C \
+# 	--outdir macs2 2> macs2/2815-C-macs2.log
+
+
 
 
 
@@ -121,6 +161,8 @@ alignBowtie2
 
 ##### FUNC CALLS #################################################################################
 
-# trim
-# align
-# sort
+#trim
+#align
+#sort
+# markDups
+ dedupBAM
